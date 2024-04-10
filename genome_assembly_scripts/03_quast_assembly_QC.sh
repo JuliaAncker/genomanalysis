183ab27085c7c32d38c20d6d8a883f9db736d981
Julia Ancker <juliaa@rackham1.uppmax.uu.se>
2024-04-10 14:10:19 +0200

    quast QC third, working

---
+++ genome_assembly_scripts/03_quast_assembly_QC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 01:00:00
+#SBATCH -J 03_01_quast
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user julia.ancker@gmail.com
+#SBATCH --output=%x.%j.out
+# Load modules
+module load bioinfo-tools
+module load python/3.7.2
+module load quast/5.0.2
+
+pilon_assembly="/home/juliaa/genomanalys/Data/Assembly/genome_assembly/pilon_assembly/pilon_assembly.fasta"
+flye_assembly_ref="/home/juliaa/genomanalys/Data/Assembly/genome_assembly/pac_bio_assembly/assembly.fasta"
+output_dir="/home/juliaa/genomanalys/Data/Assembly/assembly_QC/QC_2"
+
+quast.py $pilon_assembly -r $flye_assembly_ref -o $output_dir
+
+
