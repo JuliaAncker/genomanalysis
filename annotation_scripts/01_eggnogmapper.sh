2c258659ebda7b58f930b6f8e8e5492df1bac2bc
Julia Ancker <juliaa@rackham1.uppmax.uu.se>
2024-05-02 14:46:44 +0200

    eggnogmapper

--- annotation_scripts/01_eggnogmapper.sh
+++ annotation_scripts/01_eggnogmapper.sh
@@ -3,8 +3,8 @@
 #SBATCH -A uppmax2024-2-7
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 00:20:00
+#SBATCH -n 4
+#SBATCH -t 04:00:00
 #SBATCH -J eggnogmapper
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user julia.ancker@gmail.com
@@ -12,9 +12,12 @@
 # Load modules
 module load bioinfo-tools
 module load eggNOG-mapper/2.1.9
+# module load gffread/0.12.7
 
-input=/home/juliaa/genomanalys/Data/Assembly/genome_assembly/repeat_masker_assembly/pilon_assembly.fasta.masked
+ref_genome=/home/juliaa/genomanalys/Data/Assembly/genome_assembly/repeat_masker_assembly/pilon_assembly.fasta.masked
+genemark=/home/juliaa/genomanalys/Data/Annotation/braker_02/GeneMark-ET/genemark.gtf
 output_dir=/home/juliaa/genomanalys/Data/Annotation/eggmapper
 
 
-emapper.py -m hmmer -i $input --itype CDS -o ${output_dir}/emapper1
+# gffread $genemark -g $ref_genome -y ${output_dir}/predicted_proteins.faa
+emapper.py -i ${output_dir}/updated_predicted_proteins.faa -o ${output_dir}/emapper1 --override
