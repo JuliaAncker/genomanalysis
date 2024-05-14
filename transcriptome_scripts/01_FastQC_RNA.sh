e36f59aa5239e0c636d924d369c447f49f1b159f
Julia Ancker <juliaa@rackham1.uppmax.uu.se>
2024-05-14 16:49:58 +0200

    fastqc

--- transcriptome_scripts/01_FastQC_RNA.sh
+++ transcriptome_scripts/01_FastQC_RNA.sh
@@ -5,7 +5,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 00:20:00
-#SBATCH -J 02_FastQC_wgs
+#SBATCH -J 01_FastQC_rna
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user julia.ancker@gmail.com
 #SBATCH --output=%x.%j.out
@@ -13,8 +13,9 @@
 module load bioinfo-tools
 module load FastQC/0.11.9
 # Your commands
-
-input=/home/juliaa/genomanalys/Data/Raw_data/RNA_samples/untrimmed/SRR6040095_scaffold_10.1.fastq.gz
-output=/home/juliaa/genomanalys/Data/Assembly/transcriptome_assembly/rna_qc
+ 
+#input=/home/juliaa/genomanalys/Data/Raw_data/RNA_samples/untrimmed/SRR6040095_scaffold_10.2.fastq.gz
+input=/home/juliaa/genomanalys/Data/Assembly/transcriptome_assembly/rna_qc/trimmed/02_forward_paired_output.fastq.gz
+output=/home/juliaa/genomanalys/Data/Assembly/transcriptome_assembly/rna_qc/trimmed
 
 fastqc $input -o $output
